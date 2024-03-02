4066adc4fbd739125d38c6dd224d17ae9f42b580
NaoyaMatsunaga <>
2024-03-03 00:38:35 +0900

    MyHomePageをStatefulWidgetからStatelessWidgetに変更

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,24 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: _MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePage extends StatelessWidget {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    // setStateがないと再描画されない
+    _counter++;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('カウンター'),
       ),
       body: Center(
         child: Column(
